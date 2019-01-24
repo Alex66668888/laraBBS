d7f8a86cd361116bcf1d0079d840d1e89e66cb43
Alex66668888 <37498612+Alex66668888@users.noreply.github.com>
2019-01-25 01:49:24 +0800

    基础页面结构（增加了yarn）

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            由 <a href="http://www.drling.xin" target="_blank">Alex</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="float-right"><a href="mailto:1414818093@qq.com">联系我们</a></p>
+    </div>
+</footer>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<html lang="{{ app()->getLocale() }}">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <!-- 继承此模板的页面，如果没有定制 title 区域的话，就会自动使用第二个参数 LaraBBS 作为标题前缀。 -->
+        <title>@yield('title', 'laraBBS') - Laravel 进阶练手</title>
+
+        <!-- Styles -->
+        <!-- 根据 webpack.mix.js 的逻辑来生成 CSS 文件链接。 -->
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    </head>
+
+
+    <body>
+
+        <div id="app" class="{{ route_class() }}-page">
+
+            @include('layouts._header')
+
+            <div class="container">
+                @include('shared._messages')
+
+                <!-- 占位符声明，允许继承此模板的页面注入内容。 -->
+                @yield('content')
+
+            </div>
+
+            @include('layouts._footer')
+
+        </div>
+
+        <!-- Scripts -->
+        <script src="{{ mix('js/app.js') }}"></script>
+
+    </body>
+
+
+</html>
