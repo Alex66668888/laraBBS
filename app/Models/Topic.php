c20f9230f581e437e3b4e2576c41a389ca0ac6e7
Alex66668888 <37498612+Alex66668888@users.noreply.github.com>
2019-01-27 23:18:19 +0800

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,5 +4,31 @@ namespace App\Models;
 
 class Topic extends Model
 {
-    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    protected $fillable = [
+        'title', 'body', 'user_id', 'category_id', 'reply_count',
+        'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'
+    ];
+
+    /**
+     * 话题所对应的分类
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * 话题所对应的作者
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+
+
+
 }
