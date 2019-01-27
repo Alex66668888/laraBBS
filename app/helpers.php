0ebf19d6323c2794915a68a81acf5d5d51f7bff4
Alex66668888 <37498612+Alex66668888@users.noreply.github.com>
2019-01-28 00:42:03 +0800

    分类话题列表（安装了hieu-le/active包）

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,16 @@
 <?php
 
+/**
+ * 以路由名称作为模板的class名称，方便调整样式
+ */
 if(!function_exists('route_class')){
     function route_class(){
         return str_replace('.', '-', Route::currentRouteName());
+    }
+}
+
+if(!function_exists('category_nav_active')){
+    function category_nav_active($category_id){
+        return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
     }
 }
