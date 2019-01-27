c860e1e4595007a65f0cf02143c0fdb4b751816e
Alex66668888 <37498612+Alex66668888@users.noreply.github.com>
2019-01-27 13:22:20 +0800

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 
 class UsersController extends Controller
@@ -16,6 +16,28 @@ class UsersController extends Controller
      */
     public function show(User $user){
         return view('users.show', compact('user'));
+    }
+
+    /**
+     * 编辑显示页面
+     *
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit(User $user){
+        return view('users.edit', compact('user'));
+    }
+
+    /**
+     * 更新用户信息
+     *
+     * @param UserRequest $request
+     * @param User $user
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(UserRequest $request, User $user){
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 
 
