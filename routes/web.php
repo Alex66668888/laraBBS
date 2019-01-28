afd66aa49d5939f9e3ebbb4cb02eea05ac6908d3
Alex66668888 <37498612+Alex66668888@users.noreply.github.com>
2019-01-28 14:58:14 +0800

    html编辑器上传话题图片

--- routes/web.php
+++ routes/web.php
@@ -44,5 +44,7 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+// html 编辑器上传图片
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
